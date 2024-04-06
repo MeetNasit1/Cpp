f03a4dda5c05f60eacc02b6d0936393c5dc27351
MewtNasit1 <meetnasit918@gmail.com>
2024-04-06 17:38:28 +0530

    6-4-2024

---
+++ feet.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int feet=0;
+    int a;
+    int total=0;
+    printf("enter the feet = ");
+    scanf("%d",&a);
+     
+     total=12*feet;
+     cout<<total;
+
+}
